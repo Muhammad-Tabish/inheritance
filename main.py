43132156be4f8705678a8d22fb63f694c3e7caf6
tabish hameed <tabishhameed34@gmail.com>
2020-11-30 20:36:00 +0000

    update weekly salary with inheritance

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class working_student (student):
     def __init__(self, name, school, salary):
         super().__init__(name, school)
         self.salary = salary    
+    
+    def weekly_salary (self):
+        return self.salary * 37.5
 
 
 rolf = working_student ('tab', 'waves', 19555)
@@ -20,4 +23,4 @@ print(rolf.salary)
 print(rolf.name)
 print(rolf.school)
 
-print(f"my name is {rolf.name} i am from {rolf.school}school. i got {rolf.average()} marks in final year. my salary is {rolf.salary}")
+print(f"my name is {rolf.name} i am from {rolf.school}school. i got {rolf.average()} marks in final year. my salary is {rolf.salary} and my weekly salary is {rolf.weekly_salary()}")
